635f7e1d64cb1ea2189e55e175844c508cd814f3
Kraiphit01 <kraiphit.kg@kgmail.com>
2019-01-29 17:07:51 +0700

    mtpApp

---
+++ mtpApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mtpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
